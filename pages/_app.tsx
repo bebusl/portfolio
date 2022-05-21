9c2967650e3cafdb51ca33c095dbb4a68eac012d
bebusl <bebus1998@naver.com>
2022-05-21 17:09:39 +0900

    Feat: 스타일 정의

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import TopNav from "../component/topNav";
+import styles from "../styles/Home.module.css";
+import { useState } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [page, setPage] = useState(1);
+  return (
+    <div className={styles.container}>
+      <TopNav />
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
